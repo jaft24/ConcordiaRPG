c0b6330f72549cbe9b48571bfd6272bbef4b4be0
Jaleta Tesgera <97764183+jaft24@users.noreply.github.com>
2023-10-27 17:52:58 -0500

    Add files via upload

---
+++ Concordia RPG by Office Heroes/ConcordiaRPG/src/main/java/com/mycompany/concordiarpg/MainClass.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.concordiarpg;
+
+/**
+ *
+ * @author Office Heros
+ */
+public class MainClass {
+
+    /**
+     * As it names indicates it is the main class...
+     * We know iy doesn't have as it was suppose to 
+     * But as you can see all of the JFrames are main so that's why
+     */
+    public static void main(String[] args) {
+       // TODO code application logic here
+        
+      LoginPage Lp = new LoginPage();
+      Lp.setVisible(true); 
+       
+      // HomePage Hp = new HomePage();
+      // Hp.setVisible(true);
+        
+       // StudyConcordia Sc = new StudyConcordia();
+       // Sc.setVisible(true);
+       
+      // CampusMap Cm = new CampusMap();
+       //Cm.setVisible(true);
+       
+       // CommonQandA Cqa = new CommonQandA();
+       // Cqa.setVisible(true);
+       
+      // Assessment ass = new Assessment();
+       // ass.setVisible(true);
+       
+       
+        //StartPage sp = new StartPage();
+        //sp.setVisible(true);
+    
+ 
+    }
+    
+}
---
+++ Concordia RPG by Office Heroes/ConcordiaRPG/src/main/java/com/mycompany/concordiarpg/ScoreTracker.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.concordiarpg;
+
+/**
+ *
+ * @author Office Heros
+ */
+public class ScoreTracker {
+    
+    // variable declaration begins
+    
+    /*
+       Except for the variable score, the rest are information we obtain from the form.
+       The variable store is a static variable that tracks the questions the user answered correct.
+    */
+    public static double score = 0;
+    
+ 
+}
